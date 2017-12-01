3a026725453aceee63c18509e708cbb27023a9b1
Yolanda <ygrimalt1@gmail.com>
2017-12-01 14:09:17 +0100

    Primera prova fallida

---
+++ js/nombresPrimers.js
@@ -0,0 +1,10 @@
+function calculaPrimers (primer) {
+    for (var y; x>0; y--) {
+        if (x%y==0 || x%1==0) {
+            return true;
+        } else {
+            return false;
+        }
+
+        }
+}

c9db2725ff46a68ceedd33f9dbae02834824a6a3
Yolanda <ygrimalt1@gmail.com>
2017-12-04 13:07:47 +0100

    Prova fallida 3

--- js/nombresPrimers.js
+++ js/nombresPrimers.js
@@ -1,13 +1,12 @@
-function calculaPrimers (primer) {
+function calculaPrimers (x) {
+
+    var x, y;
+
     for (var y=0; y!=x; y++) {
-        if (x%x==0) {
-
-        } else if (x%1==0) {
-
-        } else if (x%y!=0) {
-
-        }
-        
-        }
-        return primer;
+    
+            for ( ;x%x==0 && x%1==0 && x%y!=0; x++) {
+                document.write (x);
+            }
+    
+        }  
 }
